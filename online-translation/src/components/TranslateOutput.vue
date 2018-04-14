586c7610d85bfc0478ab4237e1abbbf3e730f954
Juhnanny <18651927042@163.com>
2018-04-14 22:03:20 +0800

    在线翻译demo

---
+++ online-translation/src/components/TranslateOutput.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div id="translateOutput">
+    <h1>{{translatedText}}</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'translateOutput',
+  props:[
+  	"translatedText"
+  ]
+  
+}
+</script>
+
+<style>
+
+</style>
